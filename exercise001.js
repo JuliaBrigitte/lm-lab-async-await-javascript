5a75f7d213b1cf665d98ce4a1a190a3064a88cf8
Julia Branke <Julia.Branke@gmx.co.uk>
2022-04-07 14:27:10 +0100

    Finished exercise 1

--- exercise001.js
+++ exercise001.js
@@ -27,6 +27,36 @@ promise
       console.log("Ooops a failure!")
       //If the promise fails, write Boo. Promise rejected with response: to the console.
       // Use string interpolation to add the actual response at the end.
-      console.log(" Boo. Promise rejected with response: " + result)
+      console.log("Boo. Promise rejected with response: " + result)
     })
     .finally(() => console.log("Do some other random stuff"))
+
+
+async function testAsyncWait()
+{
+    const promiseAsync = new Promise((resolve, reject) => {
+        // Generate random number between 0 and 9
+        const randomInt = Math.floor(Math.random() * 10);
+
+        if (randomInt % 2 === 0) {
+            // Success
+            setTimeout(() => resolve("done"), 2000);
+        } else {
+            // Failure
+            setTimeout(() => reject("error"), 2000);
+        }
+    });
+    try
+    {
+        let result = await promiseAsync; // wait until the promise resolves (*)
+        console.log("Async: Yay! Promise resolved with response: " + result)
+    }
+    catch (result)
+    {
+        console.log("Async: Boo. Promise rejected with response: " + result)
+    }
+}
+
+testAsyncWait()
+
+
